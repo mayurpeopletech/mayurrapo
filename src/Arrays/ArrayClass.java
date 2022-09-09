a4eaf5e9fec32b3c1a90c1169dd758d702846d0e
mayurpeopletech <113187146+mayurpeopletech@users.noreply.github.com>
2022-09-09 19:29:22 +0530

    update

--- src/Arrays/ArrayClass.java
+++ src/Arrays/ArrayClass.java
@@ -1,5 +1,22 @@
 package Arrays;
 
-public class ArrayClass {
+import java.util.Scanner;
 
+public class ArrayClass 
+{
+    public static void main(String[] args)
+    {
+		int a[]=new int[5];
+		Scanner sc= new Scanner(System.in);
+		System.out.println(" Enter your values");
+		for(int i=0; i<5;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		System.out.println(" Output ");
+		for(int i=0;i<5;i++)
+		{
+			System.out.println(a[i]);
+		}
+	}
 }
